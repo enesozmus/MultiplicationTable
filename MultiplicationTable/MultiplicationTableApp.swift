9da9e4fc02fa1ca5444903abd2a3ae9c3d3d24b2
enesozmus <ensozmus@gmail.com>
2024-03-10 12:04:13 +0300

    Initial Commit

---
+++ MultiplicationTable/MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableApp.swift
+//  MultiplicationTable
+//
+//  Created by enesozmus on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
